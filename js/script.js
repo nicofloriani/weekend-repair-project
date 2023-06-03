4d32a4b9f4b4db93d824d904551c2f6922ab5ce7
nico_floriani <floriani.nico@gmail.com>
2023-06-03 14:00:00 -0700

    Designated vehicle objects that need repair and created event to display said vehicles when button is clicked

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+const button = document.querySelector("button");
+const repairList = document.querySelector(".need-repair");
+
 const createVehicle = function (type, numWheels, color) {
   const vehicle = {};
   vehicle.type = type;
@@ -9,7 +12,7 @@ const createVehicle = function (type, numWheels, color) {
 
 const car = createVehicle("car", 4, "blue");
 const bike = createVehicle("bike", 2, "red");
-const skateboard = createVehicle("skateboard", 4, "black");
+const skateboard = createVehicle("skateboard", 2, "black");
 
 const myVehicles = [car, bike, skateboard];
 
@@ -18,3 +21,20 @@ for (let vehicle of myVehicles) {
     console.log(key, vehicle[key]);
   }
 }
+
+bike.needsRepair = true;
+skateboard.needsRepair = true;
+
+button.addEventListener("click", function () {
+  repairList.innerHTML = "";
+
+  const vehicleRepairList = myVehicles.filter(function (vehicle) {
+    return vehicle.needsRepair === true;
+  });
+
+  for (let vehicle of vehicleRepairList) {
+    let li = document.createElement("li");
+    li.innerHTML = `My <span>${vehicle.type}</span> needs some 💜`;
+    repairList.append(li);
+  }
+});
